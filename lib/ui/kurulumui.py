0f399df24bd5a4a75002a1c026c1ceb332b34cc7
milisbir <milisarge@gmail.com>
2017-11-20 11:39:18 +0200

    kernel

--- lib/ui/kurulumui.py
+++ lib/ui/kurulumui.py
@@ -300,13 +300,15 @@ class KurulumPencere(QWidget):
         os.system('chroot ' + hedef + ' rm -f /boot/initramfs')
         os.system('chroot ' + hedef + ' rm -f /boot/kernel')
         #os.system("cp /run/initramfs/live/boot/kernel "+hedef+"/boot/kernel-$(uname -r)")
-        os.system("cp /boot/kernel* "+hedef+"/boot/")
+        os.system("cp /boot/kernel-* "+hedef+"/boot/")
         os.system('chroot ' + hedef + ' dracut --no-hostonly --add-drivers "ahci" -f /boot/initramfs')
+        os.system('chroot ' + hedef + ' ln -s /boot/kernel /boot/kernel-*')
         self.surecCubugu.setValue(100)
         self.kurulumBilgisiLabel.setText(self.tr("initrd Oluşturuldu"))
 
     def grubKur(self,hedef,baglam):
         hedef = hedef[:-1]
+        os.system("cp -f /root/bin/tamir_grub_plymouth "+baglam+"/roo/bin/")
         os.system("chroot "+baglam+" tamir_grub_plymouth")
         if hedef == "/dev/mmcblk0": #SD kart'a kurulum fix
             os.system('chroot ' + baglam + 'grub-install /dev/mmcblk0')
